63746231c92a6e1f17a898f39080e94777de05bf
xyz <mugen.xyz@gmail.com>
2025-01-31 15:09:39 +0900

    feat: add Tailwind CSS Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { comlink } from "vite-plugin-comlink";
 import Unfonts from "unplugin-fonts/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react({
       babel: {
         presets: ["jotai/babel/preset"],
