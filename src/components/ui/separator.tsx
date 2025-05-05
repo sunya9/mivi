ecb883f0d01eb43fa6cd5ed4089a4ea087c5709d
xyz <mugen.xyz@gmail.com>
2025-05-06 08:58:29 +0900

    feat: add role attribute to Separator and update Slider component for aria-labelledby

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -20,6 +20,7 @@ const Separator = React.forwardRef<
         orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
         className,
       )}
+      role="separator"
       {...props}
     />
   ),
