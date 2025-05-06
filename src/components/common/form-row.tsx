92307e5ce67186833addb48c66056a4534c576c2
xyz <mugen.xyz@gmail.com>
2025-05-06 09:01:17 +0900

    feat: enhance FormRow component with useId for accessibility

--- src/components/common/form-row.tsx
+++ src/components/common/form-row.tsx
@@ -1,13 +1,28 @@
+import { useId } from "react";
+
 interface Props {
   label: React.ReactNode;
-  controller: React.ReactNode;
+  controller:
+    | React.ReactNode
+    | ((props: { labelId: string }) => React.ReactNode);
 }
 
 export function FormRow({ label, controller }: Props) {
+  const id = useId();
+  const labelId = `${id}-label`;
   return (
-    <label className="-mx-6 my-4 flex items-center justify-between px-6">
-      <div className="flex-1">{label}</div>
-      <div className="flex-none">{controller}</div>
+    <label
+      className="-mx-6 my-4 flex items-center justify-between px-6"
+      htmlFor={labelId}
+    >
+      <div className="flex-1" id={labelId}>
+        {label}
+      </div>
+      <div className="flex-none" aria-labelledby={id}>
+        {typeof controller === "function"
+          ? controller({ labelId })
+          : controller}
+      </div>
     </label>
   );
 }
