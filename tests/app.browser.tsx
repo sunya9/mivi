444496c7f166560637a2cabadb53c30bc959a309
xyz <mugen.xyz@gmail.com>
2025-05-13 21:34:18 +0900

    feat(tests): add Vitest workspace configuration and browser tests for App component

---
+++ tests/app.browser.tsx
@@ -0,0 +1,20 @@
+import { expect, test } from "vitest";
+import { App } from "@/app";
+import { customRender } from "./util";
+import { page, userEvent } from "@vitest/browser/context";
+
+test("complete happy path", async () => {
+  // TODO: add more assertions
+  customRender(<App />);
+  await expect.element(page.getByText("MiVi")).toBeInTheDocument();
+  await userEvent.upload(
+    page.getByLabelText("Open MIDI file"),
+    "./fixtures/test.mid",
+  );
+  await userEvent.upload(
+    page.getByLabelText("Open Audio file"),
+    "./fixtures/test.mp3",
+  );
+  await page.getByRole("button", { name: "Start export" }).click();
+  await expect.element(page.getByText("Exporting…")).toBeInTheDocument();
+});
