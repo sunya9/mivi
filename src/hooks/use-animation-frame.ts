198c13424a6f9340054f8e0e198d6eeab8d2e94f
xyz <mugen.xyz@gmail.com>
2025-10-06 21:41:43 +0900

    refactor(use-animation-frame): simplify animation frame handling by using refs for callback

--- src/hooks/use-animation-frame.ts
+++ src/hooks/use-animation-frame.ts
@@ -1,22 +1,24 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export function useAnimationFrame(onAnimate: FrameRequestCallback) {
   const animationFrameRef = useRef<number>(0);
-
-  const animate: FrameRequestCallback = useCallback(
-    (timestamp) => {
-      onAnimate(timestamp);
-      animationFrameRef.current = requestAnimationFrame(animate);
-    },
-    [onAnimate],
-  );
+  const onAnimateRef = useRef(onAnimate);
 
   useEffect(() => {
+    onAnimateRef.current = onAnimate;
+  }, [onAnimate]);
+
+  useEffect(() => {
+    const animate: FrameRequestCallback = (timestamp) => {
+      onAnimateRef.current(timestamp);
+      animationFrameRef.current = requestAnimationFrame(animate);
+    };
+
     animationFrameRef.current = requestAnimationFrame(animate);
     return () => {
       if (animationFrameRef.current) {
         cancelAnimationFrame(animationFrameRef.current);
       }
     };
-  }, [animate]);
+  }, []);
 }
