c2ce293e68fd8b9f563981833388e064220ddcd7
xyz <mugen.xyz@gmail.com>
2026-01-14 21:25:20 +0900

    feat(pwa): enhance PWA support with new icons and configuration
    - Updated index.html to include new favicon and apple touch icon links.
    - Added PWA asset generation configuration in pwa-assets.config.ts.
    - Introduced multiple icon sizes for PWA in vite.config.ts.
    - Added new icon files: favicon.ico, apple-touch-icon-180x180.png, pwa-64x64.png, pwa-192x192.png, pwa-512x512.png, and maskable-icon-512x512.png.
    - Updated tsconfig.node.json to include the new PWA assets configuration file.

--- vite.config.ts
+++ vite.config.ts
@@ -47,6 +47,28 @@ export default defineConfig(({ mode }) => ({
             sizes: "any",
             type: "image/svg+xml",
           },
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
         ],
         categories: ["music", "utilities"],
         display: "standalone",
