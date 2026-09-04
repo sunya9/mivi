f06597acfa349c160e6d7cccc084831d2c8eefae
xyz <mugen.xyz@gmail.com>
2026-09-04 18:15:13 +0900

    fix: adjust slider control height for horizontal orientation

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -64,7 +64,7 @@ function Slider({
       }}
       {...props}
     >
-      <SliderPrimitive.Control className="relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-vertical:h-full data-vertical:min-h-40 data-vertical:w-auto data-vertical:flex-col">
+      <SliderPrimitive.Control className="relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-horizontal:min-h-5 data-vertical:h-full data-vertical:min-h-40 data-vertical:w-auto data-vertical:flex-col">
         <SliderPrimitive.Track
           data-slot="slider-track"
           className="relative grow overflow-hidden rounded-full bg-muted select-none data-horizontal:h-1 data-horizontal:w-full data-vertical:h-full data-vertical:w-1.5"
